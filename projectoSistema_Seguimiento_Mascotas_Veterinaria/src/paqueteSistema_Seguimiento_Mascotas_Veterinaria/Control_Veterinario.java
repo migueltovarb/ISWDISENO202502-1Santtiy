e5f78dfdcf9acd4d7b98ce4f920db969d9551083
Santiago Bustos López <santiago.bustos@campusucc.edu.co>
2025-10-08 16:58:02 -0500

    Delete projectoSistema_Seguimiento_Mascotas_Veterinaria directory

--- projectoSistema_Seguimiento_Mascotas_Veterinaria/src/paqueteSistema_Seguimiento_Mascotas_Veterinaria/Control_Veterinario.java
+++
@@ -1,8 +0,0 @@
-package paqueteSistema_Seguimiento_Mascotas_Veterinaria;
-
-public class Control_Veterinario {
-	private String fecha;
-	private int observaciones;
-	private String telefono;
-
-}
--- projectoSistema_Seguimiento_Mascotas_Veterinaria/src/paqueteSistema_Seguimiento_Mascotas_Veterinaria/Personal.java
+++
@@ -1,8 +0,0 @@
-package paqueteSistema_Seguimiento_Mascotas_Veterinaria;
-
-public class Personal {
-	public static void main(String[] args) {
-		Mascotas mascota1 = new Mascotas ("Firulais", "poodle", "5", "Juanito")
-	}
-
-}
